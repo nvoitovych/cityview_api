e1e69d7f9c52b23d8480202a880c2ed0808cfe2d
voitovych <chumba.vumbaa@gmail.com>
2019-03-25 10:53:06 +0200

    remove commented code

--- server/api/city-views/city-views.router.js
+++ server/api/city-views/city-views.router.js
@@ -10,12 +10,12 @@ const {
 const publicRouter = express.Router();
 const privateRouter = express.Router();
 
+// TODO: add e2e tests for all endpoints
 // TODO: should save uploaded images as private? and generate special link to access it?
 // TODO: how to handle images with same name? what should return if this error has occured?
 publicRouter.get('/', getCityViewList);
 publicRouter.get('/:id', validateId, getCityViewDetail);
 
-// privateRouter.post('/', validateBody, createCityView);
 privateRouter.post('/', validateCityView, validateCityViewImage, createCityView);
 
 
